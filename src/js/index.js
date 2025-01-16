65a915b7e4b6fb1319a1b876bf13a5847d082bd8
Jotanore <jotanore@hotmail.com>
2025-01-16 12:54:38 +0100

    Button functionality refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,41 @@
-
-const btnStandings = document.getElementById("standings");
-const btnResults = document.getElementById("results");
-const btnTeams = document.getElementById("teams");
+document.addEventListener('DOMContentLoaded', onDOMContentLoaded);
 
 
-const standingsBlock = document.querySelector("#standings__block");
-const resultsBlock = document.querySelector("#results__block");
-const teamsBlock = document.querySelector("#teams__block");
+function onDOMContentLoaded(e){
+    //Set the buttons
+    const btnStandings = document.getElementById("standings");
+    const btnResults = document.getElementById("results");
+    const btnTeams = document.getElementById("teams");
+    //Set the data blocks
+    const standingsBlock = document.querySelector("#standings__block");
+    const resultsBlock = document.querySelector("#results__block");
+    const teamsBlock = document.querySelector("#teams__block");
+    //Set the collections of data
+    const blocks = [standingsBlock, resultsBlock, teamsBlock];
+    const buttons = [btnStandings, btnResults, btnTeams];
+    //Set the listeners on the buttons and the actions
+    btnStandings.addEventListener("click", () => switchActiveSection(standingsBlock, btnStandings, blocks, buttons));
+    btnResults.addEventListener("click", () => switchActiveSection(resultsBlock, btnResults, blocks, buttons));
+    btnTeams.addEventListener("click", () => switchActiveSection(teamsBlock, btnTeams, blocks, buttons));
 
+}
+/**
+ * 
+ * Add hidden class to data blocks
+ * Remove the active class from buttons
+ * Show active data block by removing hidden class
+ * Set active button by adding active class
+ * 
+ * @param {Var} activeBlock 
+ * @param {Var} activeButton 
+ * @param {Array} blocks 
+ * @param {Array} buttons 
+ */
+function switchActiveSection(activeBlock, activeButton, blocks, buttons) {
 
-btnStandings.addEventListener("click", function(e){
-    standingsBlock.classList.remove("hidden");
-    resultsBlock.classList.add("hidden");
-    teamsBlock.classList.add("hidden");
+    blocks.forEach(block => block.classList.add("hidden"));
+    buttons.forEach(button => button.classList.remove("active"));
 
-    btnStandings.classList.add("active");
-    btnResults.classList.remove("active");
-    btnTeams.classList.remove("active");
-});
-btnResults.addEventListener("click", function(e){
-    standingsBlock.classList.add("hidden");
-    resultsBlock.classList.remove("hidden");
-    teamsBlock.classList.add("hidden");
-
-    btnResults.classList.add("active");
-    btnStandings.classList.remove("active");
-    btnTeams.classList.remove("active");
-});
-btnTeams.addEventListener("click", function(e){
-    standingsBlock.classList.add("hidden");
-    resultsBlock.classList.add("hidden");
-    teamsBlock.classList.remove("hidden");
-
-    btnTeams.classList.add("active");
-    btnStandings.classList.remove("active");
-    btnResults.classList.remove("active");
-});
-
+    activeBlock.classList.remove("hidden");
+    activeButton.classList.add("active");
+}
